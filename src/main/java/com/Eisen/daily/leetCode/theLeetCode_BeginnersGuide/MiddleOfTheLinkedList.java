dfab0e3a37621090949387ebb0c703f6ced3d5a0
Eisen <kanjjang89@naver.com>
2022-08-03 09:40:54 +0900

    Add: LeetCode MiddleOfTheLinkedList

---
+++ src/main/java/com/Eisen/daily/leetCode/theLeetCode_BeginnersGuide/MiddleOfTheLinkedList.java
@@ -0,0 +1,26 @@
+package com.Eisen.daily.leetCode.theLeetCode_BeginnersGuide;
+
+public class MiddleOfTheLinkedList {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public ListNode middleNode(ListNode head) {
+        return head;
+    }
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
---
+++ src/test/java/com/Eisen/daily/leetCode/theLeetCode_BeginnersGuide/MiddleOfTheLinkedListTest.java
@@ -0,0 +1,16 @@
+package com.Eisen.daily.leetCode.theLeetCode_BeginnersGuide;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MiddleOfTheLinkedListTest {
+    void testLode(){
+        MiddleOfTheLinkedList test = new MiddleOfTheLinkedList();
+        MiddleOfTheLinkedList.ListNode listNode1 = new MiddleOfTheLinkedList.ListNode(1);
+        MiddleOfTheLinkedList.ListNode listNode2 = new MiddleOfTheLinkedList.ListNode(1);
+        MiddleOfTheLinkedList.ListNode listNode3 = new MiddleOfTheLinkedList.ListNode(1);
+        MiddleOfTheLinkedList.ListNode listNode4 = new MiddleOfTheLinkedList.ListNode(1);
+        MiddleOfTheLinkedList.ListNode listNode5 = new MiddleOfTheLinkedList.ListNode(1);
+
+    }
+
+}
