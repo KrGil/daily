8f2f2ddd8f9b0f091f89498d0d3371821b1de549
Eisen <kanjjang89@naver.com>
2022-08-05 09:28:12 +0900

    Add: MiddleOfTheLinkedList

--- src/main/java/com/Eisen/daily/leetCode/theLeetCode_BeginnersGuide/MiddleOfTheLinkedList.java
+++ src/main/java/com/Eisen/daily/leetCode/theLeetCode_BeginnersGuide/MiddleOfTheLinkedList.java
@@ -12,11 +12,27 @@ public class MiddleOfTheLinkedList {
      *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      * }
      */
-
     public ListNode middleNode(ListNode head) {
-
-        return head;
+        ListNode temp = head;
+        int length = 0;
+        int mid = 0;
+        // figure length
+        while(temp != null){
+            temp = temp.next;
+            length++;
+        }
+        mid = length / 2;
+        temp = head;
+        System.out.println("temp.val = " + temp.val);
+        // return
+        while(mid != 0){
+            temp = temp.next;
+            mid--;
+        }
+        System.out.println("temp = " + temp.val);
+        return temp;
     }
+
     public static class ListNode {
         int val;
         ListNode next;
--- src/test/java/com/Eisen/daily/leetCode/theLeetCode_BeginnersGuide/MiddleOfTheLinkedListTest.java
+++ src/test/java/com/Eisen/daily/leetCode/theLeetCode_BeginnersGuide/MiddleOfTheLinkedListTest.java
@@ -1,16 +1,28 @@
 package com.Eisen.daily.leetCode.theLeetCode_BeginnersGuide;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MiddleOfTheLinkedListTest {
+    @Test
     void testLode(){
         MiddleOfTheLinkedList test = new MiddleOfTheLinkedList();
-        MiddleOfTheLinkedList.ListNode listNode1 = new MiddleOfTheLinkedList.ListNode(1);
-        MiddleOfTheLinkedList.ListNode listNode2 = new MiddleOfTheLinkedList.ListNode(1);
-        MiddleOfTheLinkedList.ListNode listNode3 = new MiddleOfTheLinkedList.ListNode(1);
-        MiddleOfTheLinkedList.ListNode listNode4 = new MiddleOfTheLinkedList.ListNode(1);
-        MiddleOfTheLinkedList.ListNode listNode5 = new MiddleOfTheLinkedList.ListNode(1);
+        MiddleOfTheLinkedList.ListNode listNode = new MiddleOfTheLinkedList.ListNode(1,
+            new MiddleOfTheLinkedList.ListNode(2,
+                new MiddleOfTheLinkedList.ListNode(3,
+                    new MiddleOfTheLinkedList.ListNode(4,
+                        new MiddleOfTheLinkedList.ListNode(5,
+                            new MiddleOfTheLinkedList.ListNode(6))))
+                )
+        );
+        // 4,5,6
+        test.middleNode(listNode);
+        System.out.println("listNode1 = " + listNode.val);
+        System.out.println("listNode1 = " + listNode.next.val);
+        System.out.println("listNode1 = " + listNode.next.next.val);
+        System.out.println("listNode1 = " + listNode.next.next.next.val);
+        System.out.println("listNode1 = " + listNode.next.next.next.next.val);
 
     }
-
 }
