931da1800ce002c7c0030ec2db6e8b5be509ede3
Eisen <kanjjang89@naver.com>
2022-08-17 21:05:55 +0900

    Add: Programmers added

---
+++ src/main/java/com/Eisen/daily/programmers/MinimumRectangle.java
@@ -0,0 +1,19 @@
+package com.Eisen.daily.programmers;
+
+/**
+ * packageName :  com.Eisen.daily.programmers
+ * fileName : MinimumRectangle
+ * author :  eisen
+ * date : 2022/08/17
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2022/08/17                eisen             최초 생성
+ */
+public class MinimumRectangle {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        return answer;
+    }
+}
