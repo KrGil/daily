4af2b7a14acbd1a9c89eae7e231b4dad2e083af8
Eisen <kanjjang89@naver.com>
2022-08-23 08:42:31 +0900

    Add: StringTest 추가

---
+++ src/main/java/com/Eisen/daily/testCoding/StringTest.java
@@ -0,0 +1,8 @@
+package com.Eisen.daily.testCoding;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String.format("");
+        System.out.println("String.format(\"\\\"id\\\" : \\\"%s\\\"\", \"id\") = " + String.format("\"id\" : \"%s\"", "id"));
+    }
+}
