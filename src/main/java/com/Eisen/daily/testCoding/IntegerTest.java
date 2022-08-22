6f8433da1c17143746140906d904fc05197de053
Eisen <kanjjang89@naver.com>
2022-08-22 20:14:39 +0900

    Add: baekjoon 10162 전자레인지 추가

---
+++ src/main/java/com/Eisen/daily/testCoding/IntegerTest.java
@@ -0,0 +1,18 @@
+package com.Eisen.daily.testCoding;
+
+/**
+ * packageName :  com.Eisen.daily.testCoding
+ * fileName : IntegerTest
+ * author :  eisen
+ * date : 2022/08/22
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2022/08/22                eisen             최초 생성
+ */
+public class IntegerTest {
+    public static void main(String[] args) {
+        System.out.println("100 / 10 = " + 100 / 10);
+    }
+}
