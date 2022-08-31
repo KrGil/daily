b5af65a107d4c034d9ce4987c34d5e33bef41e7e
Eisen <kanjjang89@naver.com>
2022-08-31 21:31:02 +0900

    Add: baekjoon main 추가

--- src/main/java/com/Eisen/daily/study/baekjoon/SomeRelations_20921.java
+++ src/main/java/com/Eisen/daily/study/baekjoon/SomeRelations_20921.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -22,9 +21,10 @@ public class SomeRelations_20921 {
         for (int i = 1; i <= N; i++) {
             list.add(i);
         }
-        // 1 2 3 4 5
+
         System.out.println("list.toString() = " + list.toString());
         int place = 0;
+        // 1 2 3 4 5
         while(K > 0){
             int last = list.remove(N-1); // 5
             int move = Math.min(K, N - 1 - place); // 7, 5 - 1 - 0 // 4
