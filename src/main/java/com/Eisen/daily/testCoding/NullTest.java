a8b2a84ae2afd8dcd44556cc9bbb8232e6d1e0cf
Eisen <kanjjang89@naver.com>
2022-09-02 17:36:35 +0900

    Add: nullTest 추가

---
+++ src/main/java/com/Eisen/daily/testCoding/NullTest.java
@@ -0,0 +1,11 @@
+package com.Eisen.daily.testCoding;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+public class NullTest {
+
+    void generateNPE(String obj) {
+        System.out.println("obj = " + obj);
+    }
+
+}
