985d3102ae9c16182bfd45563df4825885dcecdc
Eisen <kanjjang89@naver.com>
2022-10-31 12:52:49 +0900

    Modify: leetcode daily challange added

--- src/main/java/com/Eisen/daily/leetCode/easy/ToeplitzMatrix_766.java
+++ src/main/java/com/Eisen/daily/leetCode/easy/ToeplitzMatrix_766.java
@@ -4,16 +4,12 @@ import java.util.Arrays;
 
 public class ToeplitzMatrix_766 {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int[] start = matrix[0];
         for (int i = 1; i < matrix.length; i++) {
-            int[] ints = matrix[i];
-
-            for (int j = 0; j < ints.length-1; j++) {
-                if(start[j] != ints[j+1]){
+            for (int j = 0; j < matrix[i].length-1; j++) {
+                if(matrix[i-1][j] != matrix[i][j+1]){
                     return false;
                 }
             }
-            start = matrix[i];
         }
         return true;
     }
