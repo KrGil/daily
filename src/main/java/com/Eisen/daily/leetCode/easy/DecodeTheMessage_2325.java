2158203c95fd93d3fb26aaeedd549f83a0fd372d
Eisen <kanjjang89@naver.com>
2022-11-02 10:42:43 +0900

    Modify: refactor DecodeTheMessage_2325

--- src/main/java/com/Eisen/daily/leetCode/easy/DecodeTheMessage_2325.java
+++ src/main/java/com/Eisen/daily/leetCode/easy/DecodeTheMessage_2325.java
@@ -7,23 +7,21 @@ public class DecodeTheMessage_2325 {
 
     public String decodeMessage(String key, String message) {
         Map<Character, Character> mappingTable = new HashMap<>();
-        mappingTable.put(' ', ' ');
         key = key.replace(" ", "");
         char[] keyArr = key.toCharArray();
         int num = 97;
-        int cnt = 0;
-        char[] messageArr = message.toCharArray();
         for (int i = 0; i < keyArr.length; i++) {
-            if(mappingTable.containsKey(keyArr[i])){
-                continue;
-            }else{
-                mappingTable.put(keyArr[i], (char) (num+cnt));
+            if(!mappingTable.containsKey(keyArr[i])){
+                mappingTable.put(keyArr[i], (char) (num++));
             }
-            cnt++;
         }
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < message.length(); i++) {
-            sb.append(mappingTable.get(messageArr[i]));
+            if(mappingTable.containsKey(keyArr[i])){
+                sb.append(mappingTable.get(message.charAt(i)));
+            }else{
+                sb.append(message.charAt(i));
+            }
         }
         return sb.toString();
     }
