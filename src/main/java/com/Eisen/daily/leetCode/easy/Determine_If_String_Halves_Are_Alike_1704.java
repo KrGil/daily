ff688535a2e28bd88e3aca49748da99b3f190d6b
Eisen <kanjjang89@naver.com>
2022-12-01 15:50:01 +0900

    Add: daily leetCode added

---
+++ src/main/java/com/Eisen/daily/leetCode/easy/Determine_If_String_Halves_Are_Alike_1704.java
@@ -0,0 +1,34 @@
+package com.Eisen.daily.leetCode.easy;
+
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Determine_If_String_Halves_Are_Alike_1704 {
+    public boolean halvesAreAlike(String s) {
+        List<Character> vowels = List.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+        char[] sChars = s.toCharArray();
+
+        String a = s.substring(0, sChars.length / 2);
+        char[] aChars = a.toCharArray();
+
+        String b = s.substring(sChars.length / 2, sChars.length);
+        char[] bChars = b.toCharArray();
+
+        int cnt = 0;
+        for (int i = 0; i < aChars.length; i++) {
+            for (Character vowel : vowels) {
+                if(vowel == aChars[i]){
+                    cnt++;
+                }
+                if (vowel == bChars[i]) {
+                    cnt--;
+                }
+            }
+        }
+
+        return cnt == 0;
+    }
+}
---
+++ src/test/java/com/Eisen/daily/leetCode/easy/Determine_If_String_Halves_Are_Alike_1704Test.java
@@ -0,0 +1,21 @@
+package com.Eisen.daily.leetCode.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Determine_If_String_Halves_Are_Alike_1704Test {
+    Determine_If_String_Halves_Are_Alike_1704 test = new Determine_If_String_Halves_Are_Alike_1704();
+
+    @Test
+    void test(){
+        String testData = "book";
+        assertTrue(test.halvesAreAlike(testData));
+        testData = "textBook";
+        assertFalse(test.halvesAreAlike(testData));
+
+    }
+}
